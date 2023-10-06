5e38c270b9d76bd2521aecde208f0df5597e0c9a
captsubbu <143515107+captsubbu@users.noreply.github.com>
2023-10-07 01:29:08 +0900

    Add files via upload
    changes as on 6th October 2023. 2nd commit

--- js/custom.js
+++ js/custom.js
@@ -53,16 +53,152 @@
     });   
      
 
+    // MANER START
 
-    const maner_dropdown = document.getElementById('maner_expand');
-    const maner_dropdown_content = document.getElementById('maner_text_dropdown');
 
-    maner_dropdown.addEventListener('click', function() {
-    if (maner_dropdown_content.style.display === 'none') {
-        maner_dropdown_content.style.display = 'block';
+    const maner_management_dropdown = document.getElementById('maner_management_expand');
+    const maner_management_dropdown_content = document.getElementById('maner_management_text_dropdown');
+
+    maner_management_dropdown.addEventListener('click', function() {
+    if (maner_management_dropdown_content.style.display === 'none') {
+        maner_management_dropdown_content.style.display = 'block';
     } else {
-        maner_dropdown_content.style.display = 'none';
+        maner_management_dropdown_content.style.display = 'none';
     }
     });
+
+
+
+
+    const maner_adventure_dropdown = document.getElementById('maner_adventure_expand');
+    const maner_adventure_dropdown_content = document.getElementById('maner_adventure_text_dropdown');
+
+    maner_adventure_dropdown.addEventListener('click', function() {
+    if (maner_adventure_dropdown_content.style.display === 'none') {
+        maner_adventure_dropdown_content.style.display = 'block';
+    } else {
+        maner_adventure_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+
+    const maner_nature_dropdown = document.getElementById('maner_nature_expand');
+    const maner_nature_dropdown_content = document.getElementById('maner_nature_text_dropdown');
+
+    maner_nature_dropdown.addEventListener('click', function() {
+    if (maner_nature_dropdown_content.style.display === 'none') {
+        maner_nature_dropdown_content.style.display = 'block';
+    } else {
+        maner_nature_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+
+    const maner_entertainment_dropdown = document.getElementById('maner_entertainment_expand');
+    const maner_entertainment_dropdown_content = document.getElementById('maner_entertainment_text_dropdown');
+
+    maner_entertainment_dropdown.addEventListener('click', function() {
+    if (maner_entertainment_dropdown_content.style.display === 'none') {
+        maner_entertainment_dropdown_content.style.display = 'block';
+    } else {
+        maner_entertainment_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    const maner_relaxation_dropdown = document.getElementById('maner_relaxation_expand');
+    const maner_relaxation_dropdown_content = document.getElementById('maner_relaxation_text_dropdown');
+
+    maner_relaxation_dropdown.addEventListener('click', function() {
+    if (maner_relaxation_dropdown_content.style.display === 'none') {
+        maner_relaxation_dropdown_content.style.display = 'block';
+    } else {
+        maner_relaxation_dropdown_content.style.display = 'none';
+    }
+    });
+
+    // MANER END
+
+
+
+    // ACTORS START
+
+
+    const actors_adventure_dropdown = document.getElementById('actors_adventure_expand');
+    const actors_adventure_dropdown_content = document.getElementById('actors_adventure_text_dropdown');
+
+    actors_adventure_dropdown.addEventListener('click', function() {
+    if (actors_adventure_dropdown_content.style.display === 'none') {
+        actors_adventure_dropdown_content.style.display = 'block';
+    } else {
+        actors_adventure_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+
+    const actors_camping_dropdown = document.getElementById('actors_camping_expand');
+    const actors_camping_dropdown_content = document.getElementById('actors_camping_text_dropdown');
+
+    actors_camping_dropdown.addEventListener('click', function() {
+    if (actors_camping_dropdown_content.style.display === 'none') {
+        actors_camping_dropdown_content.style.display = 'block';
+    } else {
+        actors_camping_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    const actors_training_dropdown = document.getElementById('actors_training_expand');
+    const actors_training_dropdown_content = document.getElementById('actors_training_text_dropdown');
+
+    actors_training_dropdown.addEventListener('click', function() {
+    if (actors_training_dropdown_content.style.display === 'none') {
+        actors_training_dropdown_content.style.display = 'block';
+    } else {
+        actors_training_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    const actors_outdoor_dropdown = document.getElementById('actors_outdoor_expand');
+    const actors_outdoor_dropdown_content = document.getElementById('actors_outdoor_text_dropdown');
+
+    actors_outdoor_dropdown.addEventListener('click', function() {
+    if (actors_outdoor_dropdown_content.style.display === 'none') {
+        actors_outdoor_dropdown_content.style.display = 'block';
+    } else {
+        actors_outdoor_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    const actors_road_dropdown = document.getElementById('actors_road_expand');
+    const actors_road_dropdown_content = document.getElementById('actors_road_text_dropdown');
+
+    actors_road_dropdown.addEventListener('click', function() {
+    if (actors_road_dropdown_content.style.display === 'none') {
+        actors_road_dropdown_content.style.display = 'block';
+    } else {
+        actors_road_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    const actors_sight_dropdown = document.getElementById('actors_sight_expand');
+    const actors_sight_dropdown_content = document.getElementById('actors_sight_text_dropdown');
+
+    actors_sight_dropdown.addEventListener('click', function() {
+    if (actors_sight_dropdown_content.style.display === 'none') {
+        actors_sight_dropdown_content.style.display = 'block';
+    } else {
+        actors_sight_dropdown_content.style.display = 'none';
+    }
+    });
+
+
+    // ACTORS END
 
   });
